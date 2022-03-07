9c6fcc3df50bc345759e7fdb985b94d7d09b097e
Stanislav <77130336+standbit@users.noreply.github.com>
2022-03-07 15:36:06 +0300

    Get more data of salaries for statistics

--- main.py
+++ main.py
@@ -2,47 +2,56 @@ from urllib import response
 import requests
 from pprint import pprint
 import json
+from itertools import count
 
 
 def get_it_vacancies(language):
     url = "https://api.hh.ru/vacancies"
-    params = {
-        "text": "Программист",
-        "text": language,
-        "area": 1,
-        "date_from": "2022-02-07"    
-    }
-    response = requests.get(url, params=params)
-    converted_response = response.json()
-    return converted_response
+    all_vacancies = []
+    for page in count(0):
+        payload = {
+            "text": "Программист",
+            "text": language,
+            "area": 1,
+            "date_from": "2022-02-07",
+            "page": page
+            }
+        page_response = requests.get(url, params=payload)
+        page_response.raise_for_status()
+        page_content = page_response.json()
+        page_limit = page_content["pages"] -1 
+        if page >= page_limit:    # потому что вылазила ошибка, когда page=100
+            break
+        all_vacancies.append(page_content)
+    return all_vacancies
 
 
-def write_json_file(response):
-    with open("response.json", "w") as outfile:
-        json.dump(response, outfile, indent=4, ensure_ascii=False)
+# def write_json_file(response):
+#     with open("response.json", "w") as outfile:
+#         json.dump(response, outfile, indent=4, ensure_ascii=False)
 
 
-def read_json_file(json_file):
-    with open(json_file, "r") as file:
-        file_contents = file.read()
-        content = json.loads(file_contents)
-        return content
+# def read_json_file(json_file):
+#     with open(json_file, "r") as file:
+#         file_contents = file.read()
+#         content = json.loads(file_contents)
+#         return content
 
 
-def show_vacancies_num():
-    languages = ["Java", "C++", "Python", "Javascript", "Go", "Ruby", "Swift", "PHP"]
-    vacancies_num = {}
-    for language in languages:
-        response = get_it_vacancies(language)
-        vacancies_num[language] = response["found"]
-    pprint(vacancies_num)
-    return
+# def show_vacancies_num():
+#     languages = ["Java", "C++", "Python", "Javascript", "Go", "Ruby", "Swift", "PHP"]
+#     vacancies_num = {}
+#     for language in languages:
+#         response = get_it_vacancies(language)
+#         vacancies_num[language] = response["found"]
+#     pprint(vacancies_num)
+#     return
 
 
-def show_python_salaries(response):
-    for counter, vacancy in enumerate(response["items"]):
-        print(counter, vacancy["salary"])
-    return 
+# def show_python_salaries(response):
+#     for counter, vacancy in enumerate(response["items"]):
+#         print(counter, vacancy["salary"])
+#     return 
 
 
 def predict_rub_salary(vacancy):
@@ -66,20 +75,21 @@ def main():
     salary_resume = {}
     for language in languages:
         response = get_it_vacancies(language)
-        vacancies_found = response["found"]
+        vacancies_found = response[0]["found"]
         salary_sum = 0
         vacancies_processed = 0
-        for vacancy in response["items"]:
-            salary = predict_rub_salary(vacancy)
-            if salary:
-                salary_sum += salary
-                vacancies_processed += 1
-        average_salary = int(salary_sum/vacancies_processed)
-        salary_resume[language] = {
-            "vacancies_found": vacancies_found,
-            "vacancies_processed": vacancies_processed,
-            "average_salary": average_salary
-        }
+        for page in response:
+            for vacancy in page["items"]:
+                salary = predict_rub_salary(vacancy)
+                if salary:
+                    salary_sum += salary
+                    vacancies_processed += 1
+            average_salary = int(salary_sum/vacancies_processed)
+            salary_resume[language] = {
+                "vacancies_found": vacancies_found,
+                "vacancies_processed": vacancies_processed,
+                "average_salary": average_salary
+                }
     pprint(salary_resume)
 
 
